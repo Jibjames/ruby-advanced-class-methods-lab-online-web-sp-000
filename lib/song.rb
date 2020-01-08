8b62f663231f850db1fa77a47be976b196a35b38
James Bridges <masterbridges@gmail.com>
2020-01-08 03:12:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
 
   end
 
-  def self.new_from_filename(fiename)
+  def self.new_from_filename(filename)
     filename.split('.').pop.split(' - ')
     song = self.new
     song.name = filename[0]
